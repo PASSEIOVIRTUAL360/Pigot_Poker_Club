8414b6e250434713e58d654aa51b6ef1d6659573
THIAGO 360 GRAUS <108551126+PASSEIOVIRTUAL360@users.noreply.github.com>
2026-03-18 23:54:38 -0300

    3COM

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.1.11/21010
 // Filename: Pigot Poker Club.ggsk
-// Generated 2026-03-18T23:33:31
+// Generated 2026-03-18T23:54:22
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_thumbnails', 2, false, { ignoreInState: 0  });
